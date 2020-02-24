0c14507a0e4af168f7be11a1c8e9e34a4a468926
Pedro Belfort <belfortdev@gmail.com>
2020-02-24 16:00:51 +0000

    Create ApplicationWindow view

---
+++ src/main/java/madstax/view/ApplicationWindow.java
@@ -0,0 +1,29 @@
+package madstax.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ApplicationWindow extends JFrame {
+
+    private static final int WINDOW_WIDTH = 1000;
+    private static final int WINDOW_HEIGHT = 618;
+
+    public ApplicationWindow() {
+        setupWindow();
+        this.setBackground(Color.gray);
+        this.setVisible(true);
+    }
+
+    // Configures the JFrame's properties
+    private void setupWindow() {
+        // Sets the initial window size (golden ratio)
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        // Sets the window minimum dimension
+        Dimension minimumDimension = new Dimension(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+        this.setMinimumSize(minimumDimension);
+        // Sets the window location to the center of the screen
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
