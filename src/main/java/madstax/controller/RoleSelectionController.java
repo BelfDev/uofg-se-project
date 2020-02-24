48eb9ceda6a1915e561ea6f37b880e26d009dedc
Pedro Belfort <belfortdev@gmail.com>
2020-02-24 16:01:22 +0000

    Create RoleSelection Controller and Screen

---
+++ src/main/java/madstax/controller/RoleSelectionController.java
@@ -0,0 +1,11 @@
+package madstax.controller;
+
+import madstax.view.RoleSelectionScreen;
+
+public class RoleSelectionController extends Controller {
+
+    public RoleSelectionController() {
+        super(new RoleSelectionScreen());
+    }
+
+}
---
+++ src/main/java/madstax/view/RoleSelectionScreen.java
@@ -0,0 +1,14 @@
+package madstax.view;
+
+
+import java.awt.*;
+
+public class RoleSelectionScreen extends Screen {
+
+    public RoleSelectionScreen() {
+        super("Role Selection");
+        this.setBackground(Color.BLUE);
+        this.setVisible(true);
+    }
+
+}
