b769ed664abbc0363a87fd314ec82d637c5b7314
Pedro Belfort <belfortdev@gmail.com>
2020-02-24 16:02:10 +0000

    Rename the entry point class to 'Launcher'

---
+++ src/main/java/madstax/Launcher.java
@@ -0,0 +1,12 @@
+package madstax;
+
+import madstax.application.Application;
+
+/**
+ * Hello world!
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        new Application();
+    }
+}
