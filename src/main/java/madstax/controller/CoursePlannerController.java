34071c745b0e28808001526e63bb33d1a743604e
Pedro Belfort <belfortdev@gmail.com>
2020-02-24 19:26:37 +0000

    Adjust CoursePlanner scaffolding

--- src/main/java/madstax/controller/CoursePlannerController.java
+++ src/main/java/madstax/controller/CoursePlannerController.java
@@ -1,11 +1,16 @@
 package madstax.controller;
 
+import madstax.model.user.User;
 import madstax.view.CoursePlannerScreen;
 
-public class CoursePlannerController extends Controller {
+public class CoursePlannerController extends Controller<CoursePlannerScreen> {
 
-    public CoursePlannerController() {
+    private User user;
+
+    public CoursePlannerController(User user) {
         super(new CoursePlannerScreen());
+        this.user = user;
+        System.out.println(user.getRole());
     }
 
 }
--- src/main/java/madstax/view/CoursePlannerScreen.java
+++ src/main/java/madstax/view/CoursePlannerScreen.java
@@ -1,9 +1,16 @@
 package madstax.view;
 
+import java.awt.*;
+
 public class CoursePlannerScreen extends Screen {
 
     public CoursePlannerScreen() {
         super("Course Planner");
+        this.setBackground(Color.RED);
     }
 
+    @Override
+    void layoutComponents() {
+
+    }
 }
