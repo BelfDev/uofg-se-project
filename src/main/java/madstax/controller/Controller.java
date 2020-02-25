34097b08be53c7512d28b471a97d0075054856aa
Pedro Belfort <belfortdev@gmail.com>
2020-02-25 23:33:25 +0000

    Integrate NavigationBar with the ApplicationWindow and Controller

--- src/main/java/madstax/controller/Controller.java
+++ src/main/java/madstax/controller/Controller.java
@@ -1,12 +1,20 @@
 package madstax.controller;
 
-import madstax.model.navigation.Navigator;
+import madstax.controller.navigation.NavigationListener;
+import madstax.controller.navigation.Navigator;
+import madstax.view.ApplicationWindow;
 import madstax.view.Screen;
 
-public abstract class Controller<E extends Screen> {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public abstract class Controller<E extends Screen> implements NavigationListener {
 
     protected E screen;
     protected Navigator navigator;
+
+    private Timer alphaAnimator;
 
     public Controller(E screen) {
         this.screen = screen;
@@ -15,6 +23,46 @@ public abstract class Controller<E extends Screen> {
 
     public E getScreen() {
         return screen;
+    }
+
+    public void onAttached(ApplicationWindow window) {
+        window.setNavigationListener(this);
+        animateFadeIn();
+    }
+
+    public void onDetach(ApplicationWindow window) {
+        window.setNavigationListener(null);
+    }
+
+    @Override
+    public void onBackButtonClicked(ActionEvent e) {
+        System.out.println("Back Button Was Clicked from " + screen.getScreenTitle());
+        navigator.popController();
+    }
+
+    private void animateFadeIn() {
+        JPanel screen = getScreen();
+        Color bg = screen.getBackground();
+        Color initialColor = new Color(bg.getRed(), bg.getGreen(), bg.getBlue(), 0);
+        screen.setBackground(initialColor);
+        screen.repaint();
+
+        alphaAnimator = new Timer(0, e -> {
+            int incrementer = 2;
+            Color bg1 = screen.getBackground();
+            Color newColor = null;
+            if (bg1.getAlpha() >= 254) {
+                alphaAnimator.stop();
+                newColor = new Color(bg1.getRed(), bg1.getGreen(), bg1.getBlue(), 255);
+            } else {
+                int newAlpha = bg1.getAlpha() + incrementer;
+                newColor = new Color(bg1.getRed(), bg1.getGreen(), bg1.getBlue(), newAlpha);
+            }
+            screen.setBackground(newColor);
+            screen.repaint();
+        });
+
+        alphaAnimator.start();
     }
 
 }
