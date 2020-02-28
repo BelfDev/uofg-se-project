f7a1cd68610b7a4b3137939ace33a7a5e012ea1b
Pedro Belfort <belfortdev@gmail.com>
2020-02-28 18:19:00 +0000

    Add permissions to the ClassDirector role

--- src/main/java/madstax/model/user/role/ClassDirector.java
+++ src/main/java/madstax/model/user/role/ClassDirector.java
@@ -1,9 +1,21 @@
 package madstax.model.user.role;
 
+import madstax.model.user.Permission;
+
+import java.util.HashMap;
+
+import static madstax.model.user.Permission.*;
+
 class ClassDirector extends Role {
 
     public ClassDirector() {
         super("Class Director");
+        HashMap<Permission, Boolean> adminPermissions = new HashMap<Permission, Boolean>() {{
+            put(VIEW_ASSIGNED_STAFF, true);
+            put(VIEW_REQUEST_STATUS, true);
+            put(ADD_REQUIREMENT, true);
+        }};
+        this.permissions.putAll(adminPermissions);
     }
 
 }
