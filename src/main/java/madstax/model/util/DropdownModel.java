311ff32827b504f09b36d5554a24deefe0074d0a
Pedro Belfort <belfortdev@gmail.com>
2020-02-28 18:17:18 +0000

    Generify Dropdown model

---
+++ src/main/java/madstax/model/util/DropdownModel.java
@@ -0,0 +1,29 @@
+package madstax.model.util;
+
+import javax.swing.*;
+
+public class DropdownModel<E> extends DefaultComboBoxModel<E> {
+
+    private boolean introSelectionAllowed;
+
+    public DropdownModel(E[] items) {
+        super(items);
+        this.introSelectionAllowed = true;
+    }
+
+    @Override
+    public void setSelectedItem(Object anObject) {
+        if (anObject == null && introSelectionAllowed) {
+            String message = "Select an option";
+            super.setSelectedItem(message);
+        } else {
+            introSelectionAllowed = false;
+            super.setSelectedItem(anObject);
+        }
+    }
+
+    @Override
+    public E getSelectedItem() {
+        return (E) super.getSelectedItem();
+    }
+}
