4647a0444e0a7e79597d5e20233e1c1d5a4257d2
Pedro Belfort <belfortdev@gmail.com>
2020-02-29 16:59:46 +0000

    Promotes Navigator to NavigationController

--- src/main/java/madstax/controller/navigation/NavigationController.java
+++ src/main/java/madstax/controller/navigation/NavigationController.java
@@ -1,26 +1,28 @@
 package madstax.controller.navigation;
 
 import com.sun.istack.internal.NotNull;
-import madstax.controller.Controller;
+import madstax.controller.ScreenController;
 import madstax.view.ApplicationWindow;
 import madstax.view.NavigationBar;
 
 import java.util.Stack;
 
-public class Navigator {
+public class NavigationController {
 
-    private static Navigator instance = null;
+    private static NavigationController instance = null;
     private ApplicationWindow window;
-    private Stack<Controller> navigationStack;
+    private Stack<ScreenController> navigationStack;
+    private NavigationBar navigationBar;
 
-    private Navigator() {
+    private NavigationController() {
         this.navigationStack = new Stack<>();
         this.window = new ApplicationWindow();
+        this.navigationBar = window.getNavigationBar();
     }
 
-    public static Navigator getInstance() {
+    public static NavigationController getInstance() {
         if (instance == null) {
-            instance = new Navigator();
+            instance = new NavigationController();
         }
         return instance;
     }
@@ -29,43 +31,53 @@ public class Navigator {
         return window;
     }
 
-    public void setRoot(@NotNull Controller controller) {
+    public void setRoot(@NotNull ScreenController screenController) {
         if (!navigationStack.isEmpty()) {
             navigationStack.clear();
         }
-        pushController(controller);
+        pushController(screenController);
     }
 
     public void popToRoot() {
-        Controller rootController = null;
+        ScreenController rootScreenController = null;
         for (int i = 0; i < navigationStack.size(); i++) {
-            rootController = navigationStack.pop();
+            rootScreenController = navigationStack.pop();
         }
-        setRoot(rootController);
+        setRoot(rootScreenController);
     }
 
-    public void pushController(@NotNull Controller controller) {
-        navigationStack.push(controller);
-        attachToWindow(controller);
+    public void pushController(@NotNull ScreenController screenController) {
+        navigationStack.push(screenController);
+        attachToWindow(screenController);
     }
 
     public void popController() {
         if (!navigationStack.isEmpty()) {
-            navigationStack.pop().onDetach(window);
+            // Invokes 'onDetach' lifecycle method
+            navigationStack.pop().onDetach();
+            // Clears the navigation bar
+            navigationBar.setListener(null);
+            navigationBar.removeBarItems();
+            // Attaches the previous content to the window
             attachToWindow(navigationStack.peek());
         }
     }
 
-    private void attachToWindow(@NotNull Controller controller) {
-        String screenTitle = controller.getScreen().getScreenTitle();
+    private void attachToWindow(@NotNull ScreenController screenController) {
+        String screenTitle = screenController.getScreen().getScreenTitle();
         boolean isRootScreen = navigationStack.size() == 1;
         // Updates the navigation bar
-        NavigationBar navBar = window.getNavigationBar();
-        navBar.setTitleLabelText(screenTitle);
-        navBar.setBackButtonVisibility(!isRootScreen);
+        updateNavigationBar(screenTitle, !isRootScreen);
         // Attaches the content to the window
-        window.attachContent(controller.getScreen());
-        controller.onAttached(window);
+        window.attachContent(screenController.getScreen());
+        screenController.onAttached();
+
+        navigationBar.setListener(screenController);
+    }
+
+    private void updateNavigationBar(String screenTitle, boolean backButtonVisible) {
+        navigationBar.setTitleLabelText(screenTitle);
+        navigationBar.setBackButtonVisibility(backButtonVisible);
     }
 
 }
