ef7d04b514802cdb29aae42219d963701c8ed607
Pedro Belfort <belfortdev@gmail.com>
2020-02-29 20:10:48 +0000

    Extract EditorToolbar from the CoursePlannerScreen

--- src/main/java/madstax/view/CoursePlannerScreen.java
+++ src/main/java/madstax/view/CoursePlannerScreen.java
@@ -6,7 +6,6 @@ import madstax.model.RequestStatus;
 import javax.swing.*;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +15,6 @@ public class CoursePlannerScreen extends Screen {
     private static final String[] TABLE_HEADER = {"INDEX", "COURSE", "REQUIREMENTS", "TEACHER ID", "STATUS"};
 
     private JTable table;
-    private EditorToolbar editorToolbar;
-    private ModalEditor modalEditor;
 
     public CoursePlannerScreen() {
         super(COURSE_PLANNER_TITLE);
@@ -36,30 +33,6 @@ public class CoursePlannerScreen extends Screen {
         table.setDefaultRenderer(String.class, new PlannerCellRenderer());
 
         add(scrollablePane, BorderLayout.CENTER);
-    }
-
-    public <E> void setEditorToolbarType(Class<? extends E> elementType) {
-        this.editorToolbar = new EditorToolbar<E>();
-
-        // Temporary!
-        if (elementType != null) {
-            editorToolbar.setElementType(elementType);
-        } else {
-            editorToolbar.setConfirmButtonTitle("EDIT");
-        }
-
-        add(editorToolbar, BorderLayout.SOUTH);
-    }
-
-    public <E> EditorToolbar<E> getEditorToolbar() {
-        return editorToolbar;
-    }
-
-    public ModalEditor getModalEditor() {
-        if (this.modalEditor == null) {
-            this.modalEditor = new ModalEditor();
-        }
-        return modalEditor;
     }
 
     public void setTableModel(CoursePlanListModel model, ListSelectionListener selectionListener) {
@@ -81,7 +54,7 @@ public class CoursePlannerScreen extends Screen {
         table.setValueAt(requirements, selectedRow, 2);
     }
 
-    public void updateStatus(RequestStatus requestStatus) {
+    public void updateRequestStatus(RequestStatus requestStatus) {
         int selectedRow = table.getSelectedRow();
         table.setValueAt(requestStatus, selectedRow, 4);
     }
@@ -95,17 +68,8 @@ public class CoursePlannerScreen extends Screen {
         return table.getSelectedRow();
     }
 
-    public void showModalEditor(List<String> requirements) {
-        modalEditor.setRequirements(requirements);
-        modalEditor.setVisible(true);
-    }
-
-    public void setConfirmButtonListener(ActionListener listener) {
-        editorToolbar.setConfirmButtonListener(listener);
-    }
-
-    public Object getSelectedDropdownValue() {
-        return editorToolbar.getSelectedDropdownItem();
+    public void addEditorToolbar(EditorToolbar editorToolbar) {
+        add(editorToolbar, BorderLayout.SOUTH);
     }
 
 }
